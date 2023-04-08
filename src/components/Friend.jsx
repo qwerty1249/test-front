f7fdcfe82775896f30dec17a6f44c463e5e1a74a
qwerty1249 <122226253+qwerty1249@users.noreply.github.com>
2023-04-09 02:04:48 +0530

    Update Friend.jsx

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://mern-social-media-pied.vercel.app/${_id}/${friendId}`,
+      `https://embarrassed-pig-earmuffs.cyclic.app/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
